01d89def4104c0eee5d1e1d3cc0ee96323f95f8c
Fulden Özsayın <201902048@ogrenci.amasya.edu.tr>
2025-07-22 21:01:30 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movies_app/ui/cubit/home_page_cubit.dart';
 import 'package:movies_app/ui/views/home_page.dart';
 
 void main() {
@@ -10,15 +12,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context)=> HomePageCubit()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: HomePage(),
       ),
-      home: HomePage(),
     );
   }
 }
--- lib/ui/views/home_page.dart
+++ lib/ui/views/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movies_app/data/entity/movies.dart';
+import 'package:movies_app/ui/cubit/home_page_cubit.dart';
 import 'package:movies_app/ui/views/detail_page.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,35 +10,23 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  @override
+  void initState() {
+    super.initState();
+    context.read<HomePageCubit>().moviesDownload();
+  }
+
   @override
   Widget build(BuildContext context) {
 
-    Future<List<Movies>> moviesDownload() async {
-      var moviesList = <Movies>[];
-      var f1 = Movies(id: 1, name: "Django", image: "django.png", price: 24);
-      var f2 = Movies(id: 2, name: "Interstellar", image: "interstellar.png", price: 32);
-      var f3 = Movies(id: 3, name: "Inception", image: "inception.png", price: 16);
-      var f4 = Movies(id: 4, name: "The Hateful Eight", image: "thehatefuleight.png", price: 28);
-      var f5 = Movies(id: 5, name: "The Pianist", image: "thepianist.png", price: 18);
-      var f6 = Movies(id: 6, name: "Anadoluda", image: "anadoluda.png", price: 10);
-      moviesList.add(f1);
-      moviesList.add(f2);
-      moviesList.add(f3);
-      moviesList.add(f4);
-      moviesList.add(f5);
-      moviesList.add(f6);
-      return moviesList;
-    }
-
     return Scaffold(
-      appBar: AppBar(title: const Text("Detay Sayfa"),),
-      body: FutureBuilder<List<Movies>>(
-        future: moviesDownload(),
-        builder: (context, snapshot){
-          if(snapshot.hasData){
-            var moviesList=snapshot.data;
+      appBar: AppBar(title: const Text("Movies"),),
+      body: BlocBuilder<HomePageCubit,List<Movies>>(
+        builder: (context, moviesList){
+          if(moviesList.isNotEmpty){
             return GridView.builder(
-              itemCount: moviesList!.length,
+              itemCount: moviesList.length,
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 2, childAspectRatio: 1/1.8
               ),
@@ -54,8 +44,8 @@ class _HomePageState extends State<HomePage> {
                           children: [
                             Text("${movie.price}₺",style: const TextStyle(fontSize: 24),),
                             ElevatedButton(onPressed: (){
-                              print("${movie.name} sepete eklendi.");
-                            }, child: const Text("Sepet"))
+                              print("${movie.name} added to cart.");
+                            }, child: const Text("Basket"))
                           ],
                         ),
                       ],
